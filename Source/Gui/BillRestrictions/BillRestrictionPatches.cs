bd811ba64ac786c28404748fb82f5e90c88e7f71
SaberShip <sabership.noreply@github.com>
2024-03-21 00:31:41 -0500

    Organize Bill Colonist Restriction Menu

--- Source/Gui/BillRestrictions/BillRestrictionPatches.cs
+++ Source/Gui/BillRestrictions/BillRestrictionPatches.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using FloatSubMenus;
 using HarmonyLib;
 using RimWorld;
 using Verse;
@@ -16,11 +18,42 @@ namespace WhatMapIsItAnyway
 
         static void Postfix(ref IEnumerable<Widgets.DropdownMenuElement<Pawn>> __result)
         {
-            __result = from element in __result
-                where element.payload == null
-                    || (element.payload.Map != null
-                    && element.payload.Map == Find.CurrentMap)
-                select element;
+            var pawnsByMap = __result.Where(e => e.payload != null && e.payload.Map != null)
+                .GroupBy(e => e.payload.Map)
+                .ToDictionary(e => e.Key, e => e.Select(menu => menu.option).ToList());
+
+            /*if (pawnsByMap.Count < 2)
+            {
+                // Only one map, change nothing
+                return;
+            }*/
+
+            Log.Message("Making sub menus:" + pawnsByMap.Count());
+            Log.Message("Keys:" + pawnsByMap.Keys);
+
+            // Start with generic restrictions (not pawn specific)
+            var restrictions = from element in __result where element.payload == null select element;
+
+            // Add remaining restriction options organized by map for which they reside.
+            foreach (Map map in pawnsByMap.Keys)
+            {
+                var pawns = pawnsByMap[map];
+
+
+
+                if (!pawns.NullOrEmpty())
+                {
+                    Log.Message("Making sub menu map for:" + map.ToString() + pawns.Count());
+
+                    restrictions = restrictions.AddItem(new Widgets.DropdownMenuElement<Pawn>
+                    {
+                        option = FloatSubMenu.CompatCreate(map.ToString(), pawns),
+                        payload = null
+                    });
+                }
+            }
+
+            __result = restrictions;
         }
     }
 }
