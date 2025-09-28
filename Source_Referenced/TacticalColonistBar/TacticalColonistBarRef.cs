f4b4e1e458a76799db5c63c45555510ca7831aef
SaberShip <sabership.noreply@github.com>
2025-09-28 18:28:30 -0500

    1.5 Fix crashes for customized LTO Colony Groups bar

--- Source_Referenced/TacticalColonistBar/TacticalColonistBarRef.cs
+++ Source_Referenced/TacticalColonistBar/TacticalColonistBarRef.cs
@@ -21,7 +21,7 @@ namespace WhatMapIsItAnyway
             return TacticUtils.TacticalColonistBar.DrawLocs.Select(rect => rect.position).ToList();
         }
 
-        public static List<TacticalColonistBar.Entry> GetColonistBarEntries() {
+        public static List<TacticalGroups.TacticalColonistBar.Entry> GetColonistBarEntries() {
 			return TacticUtils.TacticalColonistBar.Entries;
         }
 
@@ -30,28 +30,30 @@ namespace WhatMapIsItAnyway
 			float num = 99999f;
 			float num2 = 0f;
 			float num3 = 0f;
-			List<TacticalColonistBar.Entry> entries = GetColonistBarEntries();
+			List<TacticalGroups.TacticalColonistBar.Entry> entries = GetColonistBarEntries();
 			List<Vector2> drawLocs = GetDrawLocs();
 			for (int i = 0; i < entries.Count; i++)
 			{
-				TacticalColonistBar.Entry entry = entries[i];
+                TacticalGroups.TacticalColonistBar.Entry entry = entries[i];
 				if (entry.group == group)
 				{
+
 					ColonistGroup colonistGroup = (ColonistGroup)entry.colonyGroup ?? entry.caravanGroup ?? null;
 
-					if (colonistGroup != null && entry.pawn != null)
+                    if (colonistGroup != null && entry.pawn != null)
 					{
-						Pawn pawn = entry.pawn;
-						if (!colonistGroup.pawnIcons.GetValueOrDefault(pawn).isVisibleOnColonistBar)
+
+                        Pawn pawn = entry.pawn;
+						PawnIcon icon = colonistGroup.pawnIcons.GetValueOrDefault(pawn);
+						if (icon != null && !icon.isVisibleOnColonistBar)
 						{
 							continue;
 						}
-					}
 
-
-					num = Mathf.Min(num, drawLocs[i].x);
-					num2 = Mathf.Max(num2, drawLocs[i].x + GetColonistBarSize().x);
-					num3 = Mathf.Max(num3, drawLocs[i].y + GetColonistBarSize().y);
+                        num = Mathf.Min(num, drawLocs[i].x);
+                        num2 = Mathf.Max(num2, drawLocs[i].x + GetColonistBarSize().x);
+                        num3 = Mathf.Max(num3, drawLocs[i].y + GetColonistBarSize().y);
+                    }
 				}
 			}
 			return new Rect(num, 0f, num2 - num, num3 - 0f).ContractedBy(-12f * GetColonistBarScale());
---
+++ Source_Referenced/TacticalColonistBar/TimeControlPositionPatches.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Multiplayer.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TacticalGroups;
+using UnityEngine;
+using Verse;
+
+namespace WhatMapIsItAnyway.TacticalColonistBar
+{
+    [HarmonyPatch("ColonistBarTimeControl", "DrawButtons", MethodType.Normal)]
+    public static class DrawingMpTimeControlButtons
+    {
+        public static void Prefix()
+        {
+            if (!MP.enabled || !MP.IsInMultiplayer) return;
+
+            GetGroupFrameRectPatch.DrawingTimeControls = true;
+        }
+
+        public static void Postfix()
+        {
+            if (!MP.enabled || !MP.IsInMultiplayer) return;
+
+            GetGroupFrameRectPatch.DrawingTimeControls = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ColonistBarColonistDrawer), "GroupFrameRect")]
+    public static class GetGroupFrameRectPatch
+    {
+        public static bool DrawingTimeControls = false;
+
+        public static void Postfix(ref Rect __result, int group)
+        {
+            if (DrawingTimeControls)
+            {
+                __result.x = TacticalColonistBarRef.GroupFrameRect(group).x;
+            }
+        }
+    }
+}
